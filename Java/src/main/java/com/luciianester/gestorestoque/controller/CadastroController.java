12aae23e9afd3cd25c597552550d0fa10bb3a358
lucianerm <luciianester@gmail.com>
2016-07-09 19:23:50 -0300

    Mudança para WildFly

---
+++ Java/src/main/java/com/luciianester/gestorestoque/controller/CadastroController.java
@@ -0,0 +1,19 @@
+package com.luciianester.gestorestoque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/cadastro")
+public class CadastroController {
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView model = new ModelAndView("teste/cadastro");
+		model.addObject("titulo", "Cadastro de Clientes!");
+		return model;
+	}
+
+}
---
+++ Java/src/main/webapp/WEB-INF/views/teste/cadastro.jsp
@@ -0,0 +1,15 @@
+<html>
+<body>
+	
+	<h1>${titulo}</h1>
+	<br/>
+	<input type="text" id="name" />
+	<br/>
+	<input type="number" id="age" />
+	<br/>
+	<button type="button" >Cadastrar</button>
+	
+	<jsp:include page="result.jsp"></jsp:include>
+	
+</body>
+</html>
